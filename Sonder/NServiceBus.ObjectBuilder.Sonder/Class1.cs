ce553207409dcb00f5eef2361e3b4c827b1885f3
Nevenall <nevenall@hotmail.com>
2013-10-20 13:09:27 -0700

    Initial Commit

---
+++ Sonder/NServiceBus.ObjectBuilder.Sonder/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NServiceBus.ObjectBuilder.Sonder
+{
+    public class Class1
+    {
+    }
+}
